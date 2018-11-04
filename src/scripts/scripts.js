1663b62f026314788d6d5f2cc01a81659ee6eb75
Alibek <alibekmslv@gmail.com>
2018-11-04 18:16:13 +0300

    new images added

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,13 +1,13 @@
 import MoveTo from "moveto";
 import Rellax from "rellax";
-import $ from "jquery";
+// import $ from "jquery";
 // import slick from "slick-carousel";
 // import IMask from "imask";
-import tingle from "tingle.js";
+// import tingle from "tingle.js";
 // import { lory } from "lory.js";
 import ScrollReveal from "scrollreveal";
-import "./modules/pudu-som-min";
-import "./modules/pudu-inpMask-min";
+// import "./modules/pudu-som-min";
+// import "./modules/pudu-inpMask-min";
 
 // Parallax Effects
 new Rellax(".hero_picture", {
@@ -116,109 +116,109 @@ function toggleMenu() {
 // });
 
 //Modal Window
-let modal = new tingle.modal({
-  closeMethods: ["overlay", "button", "escape"],
-  closeLabel: "Закрыть",
-  cssClass: ["modal"],
-  onOpen: function() {
-    //Form Validation
-    const name = document.getElementById("name");
-    const phone = document.getElementById("phone");
-    const submitButton = document.getElementById("contact-form-submit");
-    submitButton.disabled = true;
-    const formInputs = [name, phone];
-    const regExName = /.{2,40}/;
-    const regExPhone = /\+[7]\(\d{3}\)\d{3}\-\d{2}\-\d{2}/;
-    function checkInput() {
-      if (regExName.test(name.value) && regExPhone.test(phone.value)) {
-        submitButton.disabled = false;
-      } else {
-        submitButton.disabled = true;
-      }
-    }
+// let modal = new tingle.modal({
+//   closeMethods: ["overlay", "button", "escape"],
+//   closeLabel: "Закрыть",
+//   cssClass: ["modal"],
+//   onOpen: function() {
+//     //Form Validation
+//     const name = document.getElementById("name");
+//     const phone = document.getElementById("phone");
+//     const submitButton = document.getElementById("contact-form-submit");
+//     submitButton.disabled = true;
+//     const formInputs = [name, phone];
+//     const regExName = /.{2,40}/;
+//     const regExPhone = /\+[7]\(\d{3}\)\d{3}\-\d{2}\-\d{2}/;
+//     function checkInput() {
+//       if (regExName.test(name.value) && regExPhone.test(phone.value)) {
+//         submitButton.disabled = false;
+//       } else {
+//         submitButton.disabled = true;
+//       }
+//     }
 
-    formInputs.forEach(function(e) {
-      e.oninput = function() {
-        checkInput();
-      };
-    });
+//     formInputs.forEach(function(e) {
+//       e.oninput = function() {
+//         checkInput();
+//       };
+//     });
 
-    // let phoneMask = new IMask(phone, {
-    //   mask: "+{7} (000) 000-00-00"
-    // });
+//     // let phoneMask = new IMask(phone, {
+//     //   mask: "+{7} (000) 000-00-00"
+//     // });
 
-    const contactForm = document.getElementById("contact-form");
-    contactForm.addEventListener("submit", sendForm);
+//     const contactForm = document.getElementById("contact-form");
+//     contactForm.addEventListener("submit", sendForm);
 
-    function sendForm(e) {
-      e.preventDefault();
+//     function sendForm(e) {
+//       e.preventDefault();
 
-      //Ajax Form Sender
-      let partnerName = document.getElementById("name").value;
-      let partnerPhone = document.getElementById("phone").value;
-      let params = `partner_name=${partnerName}&partner_phone=${partnerPhone}`;
+//       //Ajax Form Sender
+//       let partnerName = document.getElementById("name").value;
+//       let partnerPhone = document.getElementById("phone").value;
+//       let params = `partner_name=${partnerName}&partner_phone=${partnerPhone}`;
 
-      let xhr = new XMLHttpRequest();
-      xhr.open("POST", "mail-script/mail.php", true);
-      xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//       let xhr = new XMLHttpRequest();
+//       xhr.open("POST", "mail-script/mail.php", true);
+//       xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-      xhr.onload = function() {
-        if (this.status == 200) {
-          contactForm.reset();
-          sayThankYou();
-          console.log("Message send");
-        } else if (this.status == 400) {
-          console.log("Error, not found");
-        }
-      };
+//       xhr.onload = function() {
+//         if (this.status == 200) {
+//           contactForm.reset();
+//           sayThankYou();
+//           console.log("Message send");
+//         } else if (this.status == 400) {
+//           console.log("Error, not found");
+//         }
+//       };
 
-      xhr.send(params);
-    }
-  },
-  onClose: function() {
-    console.log("modal closed");
-  }
-});
+//       xhr.send(params);
+//     }
+//   },
+//   onClose: function() {
+//     console.log("modal closed");
+//   }
+// });
 
-modal.setContent(`    
-<div class="popup_heading">
-  <h2>Стать партнером</h2>
-  <p>Оставьте свои контатные данные и BuyVilis свяжется с Вами</p>
-</div>
-<form id="contact-form" class="contact-form">
-  <p>
-    <label for="name">Компания</label>
-    <input type="text" id="name" name="partner_name" required>  
-  </p>
-  <p>
-    <label for="phone">Телефон</label>
-    <input type="tel" id="phone" name="partner_phone" class="pudu-inpMask" data-mask="phone_ru" required>
-  </p>
-  <p>
-    <input type="checkbox" id="checkbox-privacy" checked required><span>Ваши данные надежно защищены</span><br>
-    <label for="checkbox-privacy">Принять <a href="https://buyvilis.ru/Privacy-Policy.pdf" target="_blank">политику конфиденциальности</a></label> 
-  </p>
-<button id="contact-form-submit" type="submit" class="btn btn-no-pos">Стать партнером</button>
-</form>`);
+// modal.setContent(`
+// <div class="popup_heading">
+//   <h2>Стать партнером</h2>
+//   <p>Оставьте свои контатные данные и BuyVilis свяжется с Вами</p>
+// </div>
+// <form id="contact-form" class="contact-form">
+//   <p>
+//     <label for="name">Компания</label>
+//     <input type="text" id="name" name="partner_name" required>
+//   </p>
+//   <p>
+//     <label for="phone">Телефон</label>
+//     <input type="tel" id="phone" name="partner_phone" class="pudu-inpMask" data-mask="phone_ru" required>
+//   </p>
+//   <p>
+//     <input type="checkbox" id="checkbox-privacy" checked required><span>Ваши данные надежно защищены</span><br>
+//     <label for="checkbox-privacy">Принять <a href="https://buyvilis.ru/Privacy-Policy.pdf" target="_blank">политику конфиденциальности</a></label>
+//   </p>
+// <button id="contact-form-submit" type="submit" class="btn btn-no-pos">Стать партнером</button>
+// </form>`);
 
-let openModalBtn = document.getElementById("open-modal");
-openModalBtn.addEventListener("click", openModal);
-function openModal() {
-  modal.open();
-}
+// let openModalBtn = document.getElementById("open-modal");
+// openModalBtn.addEventListener("click", openModal);
+// function openModal() {
+//   modal.open();
+// }
 
-function sayThankYou() {
-  modal.setContent(`<div class="popup_heading popup_heading-centered">
-  <h2>Спасибо</h2>
-  <p>Мы получили Ваше сообщение и свяжемся с Вами в ближайшее время</p>
-</div>
-`);
-  setTimeout(() => {
-    modal.close();
-  }, 3000);
-}
-//Some fixes to tingle plugin
-const modalCloseButton = document.querySelector(".tingle-modal__closeIcon");
-const modalCLoseText = document.querySelector(".tingle-modal__closeLabel");
-modalCloseButton.innerHTML = `<i class="fas fa-times"></i>`;
-modalCLoseText.remove();
+// function sayThankYou() {
+//   modal.setContent(`<div class="popup_heading popup_heading-centered">
+//   <h2>Спасибо</h2>
+//   <p>Мы получили Ваше сообщение и свяжемся с Вами в ближайшее время</p>
+// </div>
+// `);
+//   setTimeout(() => {
+//     modal.close();
+//   }, 3000);
+// }
+// //Some fixes to tingle plugin
+// const modalCloseButton = document.querySelector(".tingle-modal__closeIcon");
+// const modalCLoseText = document.querySelector(".tingle-modal__closeLabel");
+// modalCloseButton.innerHTML = `<i class="fas fa-times"></i>`;
+// modalCLoseText.remove();
